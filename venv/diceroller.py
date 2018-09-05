860b1fbeb5cee820757eb96f9633dc9a0e5ee2db
warshanks <benshankles@gmail.com>
2018-09-05 17:36:07 -0500

    Added coin, d2, and d100.
    Signed-off-by: warshanks <benshankles@gmail.com>

--- venv/diceroller.py
+++ venv/diceroller.py
@@ -13,10 +13,22 @@ while runAgain == "Y":
 
     while diceNumber > 0:
 
+        if diceType == "d2":
+            print(random.randint(1, 2))
+            diceNumber -= 1
+
+        if diceType == "coin":
+            coinResult = random.randint(1, 2)
+            if coinResult == 1:
+                print("Heads")
+            else:
+                print("Tails")
+            diceNumber -= 1
+
         if diceType == "d4":
             print(random.randint(0, 4))
             diceNumber -= 1
-            runAgain = input("Would you like to roll again? [Y/N]")
+
 
         if diceType == 'd6':
             print(random.randint(0, 6))
@@ -40,8 +52,12 @@ while runAgain == "Y":
             print(random.randint(0, 20))
             diceNumber -=1
 
+        if diceType == 'd100':
+            print(random.randint(0, 100))
+            diceNumber -= 1
+
     if diceNumber == 0:
-        runAgain = input("Would you like to roll again? [Y/N]").upper()
+        runAgain = input("Would you like to roll again? [Y/N] ").upper()
         if runAgain == "N":
             quit()
 
